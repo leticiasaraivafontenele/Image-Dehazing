376d6e4bc602d988f580709fbb22a2f8c150bf6a
Ronielle Moreira <ronielle.souza63@aluno.ifce.edu.br>
2025-09-23 21:41:50 -0300

    refac binarizacao

--- src/binarizacao.py
+++ src/binarizacao.py
@@ -1,37 +1,45 @@
 import cv2
 import numpy as np
+import sys
 
-img_rgb = cv2.imread("0046_0.9_0.16.jpg")    
+if len(sys.argv) < 2:
+    print("Adicione o nome da imagem.")
+    sys.exit(1) 
+
+img_path = sys.argv[1]
+img_rgb = cv2.imread(img_path)    
+
+if img_rgb is None:
+    print(f"Erro ao carreagar a imagem '{img_path}'")
+    sys.exit(1)
 
 img = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-hist = cv2.calcHist([img], [0], None, [256], [0,256]).flatten()
-NUM = img.size
-P = hist / NUM 
+histogram = cv2.calcHist([img], [0], None, [256], [0,256]).flatten()
+pixels = img.size
+P = histogram / pixels 
 sigma2_list = []
 
 for t in range(256):
-    w1 = np.sum(P[:t+1])          # Probability sum for class 1 (intensities 0 to t)
-    w2 = np.sum(P[t+1:])          # Probability sum for class 2 (intensities t+1 to 255)
+    p_dark = np.sum(P[:t+1])
+    p_bright = np.sum(P[t+1:])
 
-    if w1 == 0 or w2 == 0:
+    if p_dark == 0 or p_bright == 0:
         sigma2_list.append(0)
         continue
 
-    u1 = np.sum(np.arange(0, t+1) * P[:t+1]) / w1  # Mean for class 1 (0 to t)
-    u2 = np.sum(np.arange(t+1, 256) * P[t+1:]) / w2  # Mean for class 2 (t+1 to 255)
+    m_dark = np.sum(np.arange(0, t+1) * P[:t+1]) / p_dark
+    m_bright = np.sum(np.arange(t+1, 256) * P[t+1:]) / p_bright
 
-    u = w1 * u1 + w2 * u2    # Total mean
+    m_total = p_dark * m_dark + p_bright * m_bright
 
-    sigma2 = w1 * (u1 - u)**2 + w2 * (u2 - u)**2
+    sigma2 = p_dark * (m_dark - m_total)**2 + p_bright * (m_bright - m_total)**2
     sigma2_list.append(sigma2)
     
 
-
-
 t_opt = np.argmax(sigma2_list)
 
-img_binary = img > t_opt  # True/False
-img_binary = img_binary.astype(np.uint8) * 255  # 0 ou 255
+img_binary = img > t_opt 
+img_binary = img_binary.astype(np.uint8) * 255
 
 cv2.imwrite("resultado_otsu.jpg", img_binary)
