4d0d5a4c76a9f01255f4b634afbcae8e74784964
guilherme <pessoamarine09@gmail.com>
2025-11-04 22:17:10 -0300

    fix-dcp

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         img_path = sys.argv[1]
     else:
-        img_path = "/home/leticia/faculdade/pdi/SOTS/outdoor/hazy/0047_0.9_0.12.jpg"
+        img_path = r"D:\dataset\SOTS\outdoor\hazy\0047_0.9_0.12.jpg"
     
     print(f"Processando: {img_path}\n")
     
